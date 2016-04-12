c2b4aec08bc46141e9e04dfeed24d55fc3757f96
n0wis <tannai.simon@gmail.com>
2016-04-12 17:14:00 +0200

    update files and delete unused files

--- Gruntfile.js
+++ Gruntfile.js
@@ -338,7 +338,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'erpApp',
+          module: 'angularApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
